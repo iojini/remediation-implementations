97808f72c64e17fba10a3f461613c8308efad07a
Irene <iojini@users.noreply.github.com>
2025-11-01 16:52:44 -0400

    Create WN10-AU-000010.ps1

---
+++ STIGS/WN10-AU-000010.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+    This PowerShell script sets the password history to 24 passwords.
+
+.NOTES
+    Author          : Irene Ojini
+    LinkedIn        : https://www.linkedin.com/in/iojini/
+    GitHub          : https://github.com/iojini
+    Date Created    : 2025-11-01
+    Last Modified   : 2025-11-01
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000010  
+
+.USAGE
+    PS C:\> .\STIG-ID-WN10-AU-000010.ps1 
+#>
+
+# Ensure the system is configured to use audit subcategories
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "SCENoApplyLegacyAuditPolicy" -Value 1 -Type DWord -Force
+
+# Enable auditing for Credential Validation (Success)
+$auditStatus = AuditPol /set /subcategory:"Credential Validation" /success:enable
+
+# Check if the command succeeded
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "Credential Validation Success auditing has been enabled."
+} else {
+    Write-Host "Failed to enable Credential Validation Success auditing. Exit code: $LASTEXITCODE"
+}
