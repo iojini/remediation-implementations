f8528670a606582c8e822a3b8c0300d4edee7a7f
Irene <iojini@users.noreply.github.com>
2025-11-01 01:47:39 -0400

    Create WN10-CC-000330.ps1

---
+++ STIGS/WN10-CC-000330.ps1
@@ -0,0 +1,32 @@
+<#
+.SYNOPSIS
+    This PowerShell script disables Basic authentication for the WinRM client.
+
+.NOTES
+    Author          : Irene Ojini
+    LinkedIn        : https://www.linkedin.com/in/iojini/
+    GitHub          : https://github.com/iojini
+    Date Created    : 2025-11-01
+    Last Modified   : 2025-11-01
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000330  
+
+.USAGE
+    PS C:\> .\STIG-ID-WN10-CC-000330.ps1 
+#>
+
+$RegPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Client"
+$ValueName = "AllowBasic"
+$ValueData = 0
+
+# Create the registry key if it doesn't exist
+if (-not (Test-Path $RegPath)) {
+    New-Item -Path $RegPath -Force | Out-Null
+}
+
+# Set the required registry value
+New-ItemProperty -Path $RegPath -Name $ValueName -Value $ValueData -PropertyType DWord -Force | Out-Null
+
+Write-Host "WinRM client Basic authentication is now disabled."
